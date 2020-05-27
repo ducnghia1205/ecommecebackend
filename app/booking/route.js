e6d8400d23d9e1999d279d10c9bcc77888c8f021
Nghia <48005173+ducnghia1205@users.noreply.github.com>
2020-05-27 14:10:45 +0700

    remove-unnessary-code (#1)
    Co-authored-by: ducnghia <nghia.hoang@dinovative.com>

--- app/booking/route.js
+++
@@ -1,7 +0,0 @@
-let controller = require('./controller');
-
-module.exports = (route)=>{
-  route.post('/book', controller.create);
-  route.get('/book', controller.getList);
-  route.get('/book/:id', controller.getDetail);
-};
--- app/user/route.js
+++ app/user/route.js
@@ -1,10 +1,7 @@
 let controller = require('./controller');
-let controllerHotel = require('../hotel/controller');
 
 module.exports = (route)=>{
   route.post('/user/login', controller.login);
   route.post('/user/register', controller.register);
   route.get('/user/:id', controller.getUser);
-  route.get('/hotel', controllerHotel.getList);
-  route.get('/hotel/:id', controllerHotel.getDetail);
 };
